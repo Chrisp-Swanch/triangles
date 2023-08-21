b1699497e0ba1898cd28d162548414c74c9b63a4
Chris Swan <chrisswan1612@gmail.com>
2023-08-21 13:24:06 +1200

    changes to the start script

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,8 @@ import triangle from './routes/triangle'
 const server = express()
 
 server.use(express.json())
-server.use('/api/v1/triangle', triangle)
+server.use('/triangle', triangle)
 
-server.use('/api/v1/*', (req, res) => res.sendStatus(404))
+server.use('/*', (req, res) => res.sendStatus(404))
 
 export default server
