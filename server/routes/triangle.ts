9e8fb88136dcb5d5c12dc800256dbaee5bb60cff
Chris Swan <chrisswan1612@gmail.com>
2023-08-21 12:46:13 +1200

    checking for invalid inputs

--- server/routes/triangle.ts
+++ server/routes/triangle.ts
@@ -5,9 +5,21 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
+    // handle funky inputs
+    console.log(`param types: a: ${typeof req.query.a} b: ${typeof req.query.b} c: ${typeof req.query.c}`)
+
     const a = Number(req.query.a)
     const b = Number(req.query.b)
     const c = Number(req.query.c)
+
+    console.log(`const types: a: ${typeof a}, ${a} b: ${typeof b}, ${b} c: ${typeof c}, ${c}`)
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+      console.log('Invalid parameters!')
+      res.status(400).json({ error: 'Invalid parameters!' })
+      return
+    }
+
     const responseData = compareSides(a, b, c)
 
     res.json({ message: responseData })
